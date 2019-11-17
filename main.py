928720f97bab34d75e5ee91c278abfd5e221b682
Kamil Wróbel <sikorka1901@gmail.com>
2019-11-17 21:18:28 +0100

    Add get_tracks function

--- main.py
+++ main.py
@@ -16,15 +16,32 @@ class NodesFetcher(object):
 
     def get_scooters(self):
         with self._driver.session() as session:
-            return session.write_transaction(self._get_scooters).single()
+            return session.write_transaction(self._get_scooters).value()
 
     def get_tracks(self):
-        with self._driver.session() as session:
-            return session.write_transaction(self._get_tracks)
+        scooters = self.get_scooters()
+        scooter_to_tracks = {}
+
+        for scooter in scooters:
+            with self._driver.session() as session:
+                stays_at_location = \
+                    session.write_transaction(self._get_ordered_stays_at_locations, scooter["carId"]).data()
+                prev = None
+                tracks = []
+                for st in stays_at_location:
+                    if prev:
+                        tracks.append({"from": prev, "to": st})
+                    prev = st
+                scooter_to_tracks[scooter["carId"]] = tracks
+
+        return scooter_to_tracks
 
     @staticmethod
     def _get_ordered_stays_at_locations(tx, scooter_id):
-        return
+        return tx.run("match (n:Scooter)-[t:STAYS_AT]->(l:Location) "
+                      "where n.carId=$scooter_id "
+                      "return properties(t) as stays_at, properties(l) as location order by t.from",
+                      scooter_id=scooter_id)
 
     @staticmethod
     def _get_data_to_check_distance_between_scooters_and_pois(tx):
@@ -34,9 +51,8 @@ class NodesFetcher(object):
 
     @staticmethod
     def _get_scooters(tx):
-        return tx.run("MATCH (n:Scooter)-[t:STAYS_AT]->(l:Location) "
-                      "RETURN n, t, l "
-                      "ORDER BY t.from")
+        return tx.run("MATCH (n:Scooter) "
+                      "RETURN properties(n)")
 
 
 def distance(coord1, coord2):
@@ -73,5 +89,7 @@ def calculate_min_max_average_difference_between_scooters_and_pois(data):
 
 a = NodesFetcher("bolt://192.168.56.102", "neo4j", "hive")
 
-print(calculate_min_max_average_difference_between_scooters_and_pois(a.get_data_to_check_distance_between_scooters_and_pois()))
+print(calculate_min_max_average_difference_between_scooters_and_pois(
+    a.get_data_to_check_distance_between_scooters_and_pois()))
+print(a.get_tracks()[12514])
 a.close()
